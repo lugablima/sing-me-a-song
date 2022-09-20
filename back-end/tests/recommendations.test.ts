7c19885df3a134a2004cd48e6adb05fa2f5f0ced
Lucas <lugablima@gmail.com>
2022-09-20 13:56:03 -0300

    chore: add the POST '/:id/upvote' route integration tests

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -1,4 +1,6 @@
 import supertest from "supertest";
+import { faker } from "@faker-js/faker";
+import { Recommendation } from "@prisma/client";
 import app from "../src/app";
 import { prisma } from "../src/database";
 import { deleteAllData, disconnectPrisma } from "./factories/scenarioFactory";
@@ -22,7 +24,9 @@ describe("POST /", () => {
 
 		const result = await server.post("/").send(recommendation);
 
-		const recommendationCreated = await prisma.recommendation.findUnique({ where: { name: recommendation.name } });
+		const recommendationCreated: Recommendation = await prisma.recommendation.findUnique({
+			where: { name: recommendation.name },
+		});
 
 		expect(result.status).toBe(201);
 		expect(recommendationCreated).not.toBeNull();
@@ -33,7 +37,7 @@ describe("POST /", () => {
 
 		const result = await server.post("/").send(recommendation);
 
-		const recommendationNotExists = await prisma.recommendation.findUnique({
+		const recommendationNotExists: Recommendation | null = await prisma.recommendation.findUnique({
 			where: { name: recommendation.name },
 		});
 
@@ -48,11 +52,33 @@ describe("POST /", () => {
 
 		const result = await server.post("/").send(recommendation);
 
-		const recommendationIsUnique = await prisma.recommendation.findMany({
+		const recommendationIsUnique: Recommendation[] = await prisma.recommendation.findMany({
 			where: { name: recommendation.name },
 		});
 
 		expect(result.status).toBe(409);
 		expect(recommendationIsUnique.length).toEqual(1);
+	});
+});
+
+describe("POST /:id/upvote", () => {
+	it("Should answer with status 200 when recommendation id exists and update score key", async () => {
+		const recommendation: CreateRecommendationData = recommendationBodyFactory.validBody();
+
+		const { id, name, score: oldScore }: Recommendation = await recommendationFactory(recommendation);
+
+		const result = await server.post(`/${id}/upvote`);
+
+		const { score: newScore }: Recommendation = await prisma.recommendation.findUnique({ where: { name } });
+
+		expect(result.status).toBe(200);
+		expect(oldScore + 1).toEqual(newScore);
+	});
+
+	it("Should answer with status 404 when recommendation id does not exist", async () => {
+		const id: number = faker.datatype.number();
+		const result = await server.post(`/${id}/upvote`);
+
+		expect(result.status).toBe(404);
 	});
 });
