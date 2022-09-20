cece8d9b7ee95a5c4f17c6dcbc73d4a36bab7190
Lucas <lugablima@gmail.com>
2022-09-20 09:30:40 -0300

    chore: add .env.example file

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}.`);
+	console.log(`Server is listening on port ${PORT}.`);
 });
