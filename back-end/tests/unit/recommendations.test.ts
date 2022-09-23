cb4c7a2803fb568702ff0e9c7f2d4579def360b3
Lucas <lugablima@gmail.com>
2022-09-23 01:28:20 -0300

    chore: adds unit tests for downvote function from recommendationsService.ts file

--- back-end/tests/unit/recommendations.test.ts
+++ back-end/tests/unit/recommendations.test.ts
@@ -69,3 +69,49 @@ describe("Upvote function", () => {
 		expect(recommendationRepository.updateScore).not.toBeCalled();
 	});
 });
+
+describe("Downvote function", () => {
+	it("Should decrease the recommendation score", async () => {
+		const recommendation: Recommendation = recommendationBodyFactory.validCompleteBody();
+		const score: number = 5;
+
+		jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendation);
+
+		jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce({ ...recommendation, score });
+
+		await recommendationService.downvote(recommendation.id);
+
+		expect(recommendationRepository.find).toBeCalled();
+		expect(recommendationRepository.updateScore).toBeCalled();
+	});
+
+	it("Should trigger an error and not update the recommendation", async () => {
+		const id: number = faker.datatype.number();
+		const expectedError: AppError = { type: "not_found", message: "" };
+
+		jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(null);
+
+		const promise = recommendationService.downvote(id);
+
+		expect(promise).rejects.toEqual(expectedError);
+		expect(recommendationRepository.find).toBeCalled();
+		expect(recommendationRepository.updateScore).not.toBeCalled();
+	});
+
+	it("Should exclude the recommendation, as the score was below -5", async () => {
+		const recommendation: Recommendation = recommendationBodyFactory.validCompleteBody();
+		const score: number = -10;
+
+		jest.spyOn(recommendationRepository, "find").mockResolvedValueOnce(recommendation);
+
+		jest.spyOn(recommendationRepository, "updateScore").mockResolvedValueOnce({ ...recommendation, score });
+
+		jest.spyOn(recommendationRepository, "remove").mockResolvedValueOnce();
+
+		await recommendationService.downvote(recommendation.id);
+
+		expect(recommendationRepository.find).toBeCalled();
+		expect(recommendationRepository.updateScore).toBeCalled();
+		expect(recommendationRepository.remove).toBeCalled();
+	});
+});
